66234c39f4d7aa78bfa8480a0230b84910971313
Pankaj <panakajyadav@pixabits.in>
2024-09-17 13:03:44 +0530

    chatbot

--- src/app/components/websiteComps/products/all-products/all-products.component.ts
+++ src/app/components/websiteComps/products/all-products/all-products.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-all-products',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './all-products.component.html',
   styleUrl: './all-products.component.scss'
 })
