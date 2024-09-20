26ba1176c896ff751d76e2f65c83591a71b301f3
Pankaj <panakajyadav@pixabits.in>
2024-09-20 18:20:12 +0530

    date and time

--- src/app/components/websiteComps/demo-request/demo-request.component.ts
+++ src/app/components/websiteComps/demo-request/demo-request.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AlertService} from "../../../services/alert.service";
 import {NavbarComponent} from "../navbar/navbar.component";
@@ -19,78 +19,99 @@ import {WebsiteService} from "../website.service";
   templateUrl: './demo-request.component.html',
   styleUrl: './demo-request.component.scss'
 })
-export class DemoRequestComponent {
+export class DemoRequestComponent implements OnInit {
 
   requestForm: FormGroup;
+  minDate!: string;
+  isPastDateSelected = false;
 
-  constructor(private formBuilder: FormBuilder, public alertService: AlertService, public shredService : SharedService, private websiteService : WebsiteService) {
+  constructor(private formBuilder: FormBuilder, public alertService: AlertService, public shredService: SharedService, private websiteService: WebsiteService) {
     this.requestForm = this.formBuilder.group({
       name: new FormControl('', [Validators.required]),
       mobile: new FormControl('', [
         Validators.required,
         Validators.minLength(10),
         Validators.maxLength(10),
-        Validators.pattern('^[0-9]*$')]), // Optional: Ensure only numbers are allowed
+        Validators.pattern('^[0-9]*$')
+      ]),
       email: new FormControl('', [Validators.required, Validators.email]),
       message: new FormControl('', [Validators.required]),
+      demoDate: [null, Validators.required],
+      demoTime: [null, Validators.required],
       wapNotification: [false],
       connectExpert: [false, Validators.requiredTrue],
       keepInformed: [false]
-    })
+    });
+  }
+
+  ngOnInit() {
+    const today = new Date();
+    this.minDate = today.toISOString().split('T')[0];  // yyyy-mm-dd format for HTML5 date input
+
+    this.requestForm.get('demoDate')?.valueChanges.subscribe(date => {
+      if (date) {
+        this.checkIfPastDateSelected(date);
+      }
+    });
+  }
+
+  formatTimeTo12Hour(time: string): string {
+    const [hourString, minute] = time.split(':');
+    let hour = parseInt(hourString, 10);
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+
+    // Convert 24-hour format to 12-hour format
+    hour = hour % 12;
+    hour = hour ? hour : 12; // If hour is 0, make it 12
+
+    const formattedTime = `${('0' + hour).slice(-2)}:${minute} ${ampm}`;
+    return formattedTime;
+  }
+
+  checkIfPastDateSelected(date: string) {
+    const today = new Date();
+    const selectedDate = new Date(date);
+
+    // If the selected date is in the past, disable the time input
+    this.isPastDateSelected = selectedDate < today;
+
+    if (this.isPastDateSelected) {
+      this.requestForm.get('demoTime')?.reset();
+      this.requestForm.get('demoTime')?.disable();
+    } else {
+      this.requestForm.get('demoTime')?.enable();
+    }
   }
 
   submit() {
-
-
-    if (this.requestForm.value.name == null || this.requestForm.value.name == '') {
-      this.alertService.showAlert("please enter full name", "dark")
-      return;
-    }
-    if (this.requestForm.value.email == null || this.requestForm.value.email == '') {
-      this.alertService.showAlert("please enter valid email", "dark")
-      return;
-    }
-    if (this.requestForm.value.message == null || this.requestForm.value.message == '') {
-      this.alertService.showAlert("please enter a message ", "dark")
+    if (this.requestForm.invalid) {
+      this.alertService.showAlert("Please enter valid details", "dark");
       return;
     }
 
     if (this.requestForm.value.connectExpert == false) {
-      this.alertService.showAlert(" You must agree to connect with an expert.", "dark")
+      this.alertService.showAlert("You must agree to connect with an expert.", "dark");
       return;
     }
 
-    if (this.requestForm.invalid) {
-      this.alertService.showAlert("please enter valid details", "dark")
-      return;
-    }
+    const formValue = this.requestForm.value;
+    formValue.demoTime = this.formatTimeTo12Hour(formValue.demoTime);
 
-    console.log("request form {}", this.requestForm.value);
-    // this.alertService.showAlert("Demo requested successfully", "success");
-
+    // console.log("request form", this.requestForm.value);
 
     this.websiteService.demoRequestNew(this.requestForm.value).subscribe({
-      next : (response: any) => {
+      next: (response: any) => {
         if (response.statusCode == 200) {
           this.alertService.showApiAlert(response.message, "success");
           setTimeout(() => {
             this.requestForm.reset();
             window.location.href = '/web';
           }, 3000);
-
         }
       },
       error: (error: any) => {
         this.alertService.showApiAlert(error.message, "danger");
       }
     });
-
-
-
-  }
-
-
-  testAlert() {
-    this.alertService.showApiAlert("Details saved successfully", "error");
   }
 }
